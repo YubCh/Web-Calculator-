de13a17d4706982f9600a1e0c634974b7847276e
Yu <ge75qeb@mytum.de>
2023-02-13 21:42:17 +0100

    script change

--- script.js
+++ script.js
@@ -4,22 +4,21 @@ const result = document.querySelector('#result');
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    const buttonValue = e.target.textContent;
-    switch (buttonValue) {
-      case 'C':
-        expression.textContent = '';
-        result.textContent = '';
-        break;
-      case '=':
-        try {
-          result.textContent = eval(expression.textContent);
-        } catch (error) {
-          result.textContent = 'Error';
-        }
-        break;
-      default:
-        expression.textContent += buttonValue;
-        break;
+    const value = e.target.innerText;
+
+    if (value === 'C') {
+      expression.innerText = '';
+      result.innerText = '';
+    } else if (value === '=') {
+      try {
+        expression.innerText = eval(expression.innerText);
+      } catch (error) {
+        result.innerText = 'Error';
+      }
+    } else if (value === '&larr;') {
+      expression.innerText = expression.innerText.slice(0, -1);
+    } else {
+      expression.innerText += value;
     }
   });
 });
